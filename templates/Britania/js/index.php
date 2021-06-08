e9a370e106be82bbc6a09b9a8d896fa2db44ba06
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-06-08 15:13:16 +0200

    Fix bugs
    Fixed many bugs

--- templates/Britania/js/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('location:../'.$lien_440.'');
-?>
---
+++ update.sql
@@ -0,0 +1,2 @@
+-- CMS Settings, add new features settings
+INSERT INTO `cms_settings` (`id`, `setting`, `default_value`, `description`) VALUES (NULL, 'tinymce_key', 'no-api-key', 'Key for use editor in administration'), (NULL, 'max_level', '100', 'Max level on your game');
