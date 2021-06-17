6975875b1dd8b8f9271dfe90029bbd850d89abea
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-06-17 18:04:38 +0200

    Update for code qualitty

--- public/assets/themes/BritaniaR/js/players.js
+++ public/assets/themes/BritaniaR/js/players.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    let all_players = document.querySelector('#all');
-    let online_players = document.querySelector('#online');
-    let content = document.querySelector('#content-page');
-    let links = document.querySelectorAll('.page-link');
+    let allPlayers = document.querySelector("#all");
+    let onlinePlayers = document.querySelector("#online");
+    let content = document.querySelector("#content-page");
+    let links = document.querySelectorAll(".page-link");
 
     loadContentDefault();
 
-    if (all_players != null) {
-        all_players.addEventListener('click', () => {
+    if (allPlayers != null) {
+        allPlayers.addEventListener("click", () => {
             // Quand on clique sur Tout les joueurs
-            online_players.classList.remove('active');
-            all_players.classList.add('active');
+            onlinePlayers.classList.remove("active");
+            allPlayers.classList.add("active");
             loadContentDefault();
         })
     }
 
-    if (online_players != null) {
-        online_players.addEventListener('click', () => {
+    if (onlinePlayers != null) {
+        onlinePlayers.addEventListener("click", () => {
             // Quand on clique sur Tout les joueurs
-            online_players.classList.add('active');
-            all_players.classList.remove('active');
+            onlinePlayers.classList.add("active");
+            allPlayers.classList.remove("active");
             loadContentOnline();
         })
     }
@@ -30,21 +30,21 @@ $(document).ready(function () {
         <span class="sr-only">Loading...</span>
       </div></div>`;
         let formData = new FormData();
-        formData.append('ajax', 'all');
-        formData.append('page', page)
+        formData.append("ajax", "all");
+        formData.append("page", page)
         fetch(window.location.href, {
-            method: 'POST',
+            method: "POST",
             body: formData
         }).then(function (response) {
             return response.json();
         }).then(function (data) {
             if (data.success) {
                 content.innerHTML = data.content;
-                links = document.querySelectorAll('.page-link');
+                links = document.querySelectorAll(".page-link");
                 console.log(links)
                 if (links != null) {
-                    links.forEach(link => {
-                        link.addEventListener('click', (e) => {
+                    links.forEach((link) => {
+                        link.addEventListener("click", (e) => {
                             let page = e.target;
                             loadContentDefault(page.innerText);
                         })
@@ -60,9 +60,9 @@ $(document).ready(function () {
         <span class="sr-only">Loading...</span>
       </div></div>`;
         let formData = new FormData();
-        formData.append('ajax', 'online');
+        formData.append("ajax", "online");
         fetch(window.location.href, {
-            method: 'POST',
+            method: "POST",
             body: formData
         }).then(function (response) {
             return response.json();
