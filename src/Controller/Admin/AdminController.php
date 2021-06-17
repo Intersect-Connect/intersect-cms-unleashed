18a7bfbc9aaeac441c5539f10f967e11b53492d5
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-06-17 23:51:13 +0200

    Security update

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -13,6 +13,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ *
+ * or use @Security for more flexibility:
+ *
+ * @Security("is_granted('ROLE_ADMIN')")
+ */
 
 class AdminController extends AbstractController
 {
@@ -66,7 +76,7 @@ class AdminController extends AbstractController
             $server_info = null;
         }
 
-                //    $total_users['Total'] != null ? $total_users['Total'] : null;
+        //    $total_users['Total'] != null ? $total_users['Total'] : null;
 
 
         return $this->render($settings->get('theme') . '/admin/index.html.twig', [
