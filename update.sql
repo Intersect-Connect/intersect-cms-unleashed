5c22ce562a7d14c0748c44b782fef2fbe79d67d1
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-07-01 12:57:26 +0200

    Update update.sql

--- update.sql
+++ update.sql
@@ -6,7 +6,7 @@ ALTER TABLE `cms_pages` CHANGE `content` `content` LONGTEXT CHARACTER SET utf8mb
 
 -- CMS News
 ALTER TABLE `cms_news` CHANGE `content` `content` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
-ALTER TABLE `cms_news` ADD `category_id` int(11)
+ALTER TABLE `cms_news` ADD `category_id` int(11);
 
 
 -- --------------------------------------------------------
@@ -19,6 +19,6 @@ CREATE TABLE `cms_news_category` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 ALTER TABLE `cms_news_category` ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
-ALTER TABLE cms_news ADD CONSTRAINT FK_DA45342D12469DE2 FOREIGN KEY (category_id) REFERENCES cms_news_category (id)
+ALTER TABLE cms_news ADD CONSTRAINT FK_DA45342D12469DE2 FOREIGN KEY (category_id) REFERENCES cms_news_category (id);
 ALTER TABLE `cms_news_category` ADD `color` VARCHAR(255) NOT NULL AFTER `name`;
 -- --------------------------------------------------------
