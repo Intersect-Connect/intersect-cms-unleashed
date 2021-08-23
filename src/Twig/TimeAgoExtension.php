3e5462a976fb4c5f78aa33b898042249c70e9809
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-08-23 17:33:22 +0200

    Add translation for Time Ago Extension
    Thank's Worldofjimmy of https://game.crelasia.com/

--- src/Twig/TimeAgoExtension.php
+++ src/Twig/TimeAgoExtension.php
@@ -2,25 +2,48 @@
 
 namespace App\Twig;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 
 class TimeAgoExtension extends AbstractExtension
 {
+        private $request;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->request = $requestStack;
+    }
+
     public function getFilters()
     {
         return [
             new TwigFilter('ago', [$this, 'timeAgo']),
-             new TwigFilter('agoTwo', [$this, 'timeAgoTwo']),
+            new TwigFilter('agoTwo', [$this, 'timeAgoTwo']),
         ];
     }
 
     public function timeAgo($seconds)
     {
+        $request = $this->request->getCurrentRequest();
+        $locale = $request->getLocale();
         $seck = round($seconds);
         $dtF = new \DateTime('@0');
         $dtT = new \DateTime("@$seck");
-        return $dtF->diff($dtT)->format('%a jour(s), %h heure(s), %i minute(s), %s seconde(s)');
+
+        switch ($locale) {
+            case "fr":
+                return $dtF->diff($dtT)->format('%a jour(s), %h heure(s), %i minute(s), %s seconde(s)');
+                break;
+            case "en":
+                return $dtF->diff($dtT)->format('%a day(s), %h hour(s), %i minute(s), %s second(s)');
+                break;
+            case "es":
+                return $dtF->diff($dtT)->format('%a dia(s), %h hora(s), %i minuto(s), %s segundo(s)');
+                break;
+                default:
+        return $dtF->diff($dtT)->format('%a day(s), %h hour(s), %i minute(s), %s second(s)');
+        }
     }
 }
