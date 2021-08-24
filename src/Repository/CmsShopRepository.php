aaa11a05ad7f33d3e64d3e517669fe24d537a734
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-08-24 20:29:43 +0200

    Add version signature in all php files

--- src/Repository/CmsShopRepository.php
+++ src/Repository/CmsShopRepository.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Intersect CMS Unleashed
+ * 2.2 Update
+ * Last modify : 24/08/2021 at 20:21
+ * Author : XFallSeane
+ * Website : https://intersect.thomasfds.fr
+ */
+
 namespace App\Repository;
 
 use App\Entity\CmsShop;
